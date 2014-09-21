c2dcb99ca54a0bf48ea958390fed353c8ae5f8b5
Cody Collins <snillocydoc@yahoo.com>
2014-09-21 12:50:43 -0400

    Added error handling for minet_socket()

--- http_client.cc
+++ http_client.cc
@@ -9,6 +9,7 @@
 #include <ctype.h>
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netdb.h>
 
 
@@ -16,20 +17,27 @@
 
 int main(int argc, char * argv[]) {
 
+   
     char * server_name = NULL;
+    fd_set fd;
     struct hostent *host=(struct hostent*)malloc(sizeof(struct hostent));
     struct sockaddr_in myaddr;
     int server_port    = -1;
+    int socket;
     char * server_path = NULL;
     char * req         = NULL;
+    char * buf         = (char *)malloc(20);
     bool ok            = false;
-
+	
+	
+	
     /*parse args */
     if (argc != 5) {
 	fprintf(stderr, "usage: http_client k|u server port path\n");
 	exit(-1);
     }
-
+	
+	
     server_name = argv[2];
     server_port = atoi(argv[3]);
     server_path = argv[4];
@@ -50,12 +58,15 @@ int main(int argc, char * argv[]) {
     }
 
     /* make socket */
-	int socket=minet_socket(SOCK_STREAM);
-
+	if((socket=minet_socket(SOCK_STREAM))<0){
+		fprintf(stderr, "First argument must be k or u\n");
+		exit(-1);
+	}
+	
     /* get host IP address  */
     /* Hint: use gethostbyname() */
 	host=gethostbyname(server_name);
-
+	
     /* set address */
 	myaddr.sin_family=AF_INET;
 	memcpy(&myaddr.sin_addr.s_addr,host->h_addr,host->h_length);
@@ -63,16 +74,20 @@ int main(int argc, char * argv[]) {
 
     /* connect to the server socket */
 	minet_connect(socket, (struct sockaddr_in*)&myaddr);
-
+	
     /* send request message */
     sprintf(req, "GET %s HTTP/1.0\r\n\r\n", server_path);
 
     /* wait till socket can be read. */
     /* Hint: use select(), and ignore timeout for now. */
-	minet_select(1,NULL,NULL,NULL,NULL);
+	FD_ZERO(&fd);
+	FD_SET(socket,&fd);
+	minet_select(1,&fd,NULL,NULL,NULL);
+//fflush(stdout);
     /* first read loop -- read headers */
-
-    /* examine return code */   
+	minet_read(socket, buf, 20);
+    /* examine return code */  
+	 
 
     //Skip "HTTP/1.0"
     //remove the '\0'
@@ -80,7 +95,8 @@ int main(int argc, char * argv[]) {
     // Normal reply has return code 200
 
     /* print first part of response: header, error code, etc. */
-
+	buf[19]=0;
+	printf("received: %s",buf);
     /* second read loop -- print out the rest of the response: real web content */
 
     /*close socket and deinitialize */
